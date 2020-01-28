331b73aa2383c631f8fe55541f6dff2ff7b678b4
fishb6nes <fishb6nes@users.noreply.github.com>
2020-01-28 20:57:20 +0100

    Remove unnecessary implicit param list from onEnable

--- chat-spigot/src/main/scala/gg/warcraft/chat/spigot/ChatPlugin.scala
+++ chat-spigot/src/main/scala/gg/warcraft/chat/spigot/ChatPlugin.scala
@@ -1,32 +1,18 @@
 package gg.warcraft.chat.spigot
 
-import com.typesafe.config.Config
 import gg.warcraft.chat.channel.ChannelService
 import gg.warcraft.chat.message.MessageAdapter
 import gg.warcraft.chat.profile.ChatProfileService
 import gg.warcraft.chat.{ChatConfig, ChatService}
-import gg.warcraft.monolith.api.core.event.EventService
-import gg.warcraft.monolith.api.core.{AuthorizationService, TaskService}
 import gg.warcraft.monolith.spigot.Implicits._
-import org.bukkit.Server
 import org.bukkit.plugin.java.JavaPlugin
 
 class ChatPlugin extends JavaPlugin {
   override def onLoad(): Unit = saveDefaultConfig()
 
-  override def onEnable(): Unit = init(
-    yamlMapper.readValue(getConfig.toString, classOf[ChatConfig])
-  )
+  override def onEnable(): Unit = {
+    val config = yamlMapper.readValue(getConfig.toString, classOf[ChatConfig])
 
-  private def init(
-      config: ChatConfig
-  )(
-      implicit dbConfig: Config,
-      server: Server,
-      authService: AuthorizationService,
-      eventService: EventService,
-      taskService: TaskService
-  ): Unit = {
     implicit val messageAdapter: MessageAdapter = new SpigotMessageAdapter
     implicit val channelService: ChannelService = new ChannelService
     implicit val profileService: ChatProfileService = new ChatProfileService
