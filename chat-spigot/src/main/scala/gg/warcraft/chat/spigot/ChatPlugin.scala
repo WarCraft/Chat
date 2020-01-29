aad5d63792aa078eb2bbcf85685c973b396190d0
fishb6nes <fishb6nes@users.noreply.github.com>
2020-01-29 12:53:25 +0100

    Clean up config parsing

--- chat-spigot/src/main/scala/gg/warcraft/chat/spigot/ChatPlugin.scala
+++ chat-spigot/src/main/scala/gg/warcraft/chat/spigot/ChatPlugin.scala
@@ -5,21 +5,27 @@ import gg.warcraft.chat.message.MessageAdapter
 import gg.warcraft.chat.profile.ChatProfileService
 import gg.warcraft.chat.{ChatConfig, ChatService}
 import gg.warcraft.monolith.spigot.Implicits._
+import io.circe.parser._
 import org.bukkit.plugin.java.JavaPlugin
 
 class ChatPlugin extends JavaPlugin {
+  implicit val playerQueryService = null // TODO add to monolith implicits
+
   override def onLoad(): Unit = saveDefaultConfig()
 
   override def onEnable(): Unit = {
-    val config = yamlMapper.readValue(getConfig.toString, classOf[ChatConfig])
-
     implicit val messageAdapter: MessageAdapter = new SpigotMessageAdapter
     implicit val channelService: ChannelService = new ChannelService
     implicit val profileService: ChatProfileService = new ChatProfileService
     implicit val chatService: ChatService = new ChatService
 
-    channelService.readConfig(config)
-    profileService.readConfig(config)
+    decode[ChatConfig](getConfig.saveToString()) match {
+      case Left(err) =>
+        getLogger.severe("Failed to parse configuration: " + err.getMessage)
+      case Right(config) =>
+        channelService.readConfig(config)
+        profileService.readConfig(config)
+    }
 
     eventService.subscribe(profileService)
     eventService.subscribe(chatService)
