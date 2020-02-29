a4f3f4dbb617b97e66a95a77d2b4f8b1f95ea655
fishb6nes <fishb6nes@users.noreply.github.com>
2020-02-29 12:06:05 +0000

    Add missing Circe codecs

--- chat-spigot/src/main/scala/gg/warcraft/chat/spigot/ChatPlugin.scala
+++ chat-spigot/src/main/scala/gg/warcraft/chat/spigot/ChatPlugin.scala
@@ -1,24 +1,26 @@
 package gg.warcraft.chat.spigot
 
+import gg.warcraft.chat.{ChatConfig, ChatService}
 import gg.warcraft.chat.channel.ChannelService
 import gg.warcraft.chat.message.MessageAdapter
 import gg.warcraft.chat.profile.ChatProfileService
-import gg.warcraft.chat.{ChatConfig, ChatService}
+import gg.warcraft.monolith.spigot.Codecs.Circe._
 import gg.warcraft.monolith.spigot.Implicits._
+import io.circe.generic.auto._
 import io.circe.parser._
 import org.bukkit.plugin.java.JavaPlugin
 
 class ChatPlugin extends JavaPlugin {
-  implicit val playerQueryService = null // TODO add to monolith implicits
-
   override def onLoad(): Unit = saveDefaultConfig()
 
   override def onEnable(): Unit = {
-    implicit val messageAdapter: MessageAdapter = new SpigotMessageAdapter
+    // chat services
     implicit val channelService: ChannelService = new ChannelService
     implicit val profileService: ChatProfileService = new ChatProfileService
     implicit val chatService: ChatService = new ChatService
 
+    // parse config
+    implicit val messageAdapter: MessageAdapter = new SpigotMessageAdapter
     decode[ChatConfig](getConfig.saveToString()) match {
       case Left(err) =>
         getLogger.severe("Failed to parse configuration: " + err.getMessage)
@@ -27,6 +29,7 @@ class ChatPlugin extends JavaPlugin {
         profileService.readConfig(config)
     }
 
+    // init
     eventService.subscribe(profileService)
     eventService.subscribe(chatService)
 
