b8d8712842ca6083ff81aa8f36d36c42a9e4430d
fishb6nes <fishb6nes@users.noreply.github.com>
2020-03-09 23:57:38 +0000

    Move Akka classes into separate project

---
+++ chat-akka/src/main/scala/gg/warcraft/chat/akka/ChatService.scala
@@ -0,0 +1,61 @@
+package gg.warcraft.chat.akka
+
+import java.util.UUID
+
+import akka.actor.typed.{ ActorRef, Behavior }
+import akka.actor.typed.scaladsl.Behaviors
+
+object ChatService {
+  sealed trait Command
+
+  /** */
+  final case class RegisterHandler(
+      playerId: UUID,
+      replyTo: ActorRef[PlayerInput]
+  ) extends Command
+
+  /** */
+  final case class PlayerInput(input: Option[String])
+
+  final case class RouteMessage(player: Player,  text: String, channel: Option[String] = None) extends Command
+
+  /** */
+  def apply(handlers: Map[UUID, ActorRef[PlayerInput]]): Behavior[Command] =
+    Behaviors.receive { (context, message) =>
+      message match {
+        case RegisterHandler(playerId, replyTo) =>
+          ChatService(handlers + (playerId -> replyTo))
+
+        case RouteMessage(player, text) =>
+          /*
+           handlers.get(playerId) match {
+        case Some(handler) =>
+          // run on next sync tick as chat events are async
+          taskService.runNextTick(() => {
+            if (handler.handle(playerId, text)) handlers.subtractOne(playerId)
+          })
+
+        case None =>
+          var trimmedText = text
+          val channel = channelsByShortcut.find(it => text.startsWith(it._1)) match {
+            case Some((_, channel)) =>
+              trimmedText = text.substring(channel.shortcut.get.length).trim
+              channel
+
+            case None =>
+              channelsByName.getOrElse(
+                profiles(playerId).home,
+                defaultChannel
+              )
+          }
+
+          val command = channel.name.toLowerCase
+          channel.handle(
+            CommandSender(name, Some(playerId)),
+            Command(command, command, text.split(" "))
+          )
+      }
+           */
+      }
+    }
+}
---
+++ chat-akka/src/main/scala/gg/warcraft/chat/akka/ChatSystem.scala
@@ -0,0 +1,58 @@
+package gg.warcraft.chat.akka
+
+import java.util.Properties
+
+import akka.actor.typed.{ ActorRef, Behavior, PostStop }
+import akka.actor.typed.scaladsl.Behaviors
+import com.typesafe.config.ConfigFactory
+import gg.warcraft.chat.akka.channel.{ GlobalChannel, LocalChannel }
+import gg.warcraft.chat.akka.profile.ProfileService
+import gg.warcraft.monolith.api.core.command.CommandService
+import gg.warcraft.monolith.api.core.event.EventService
+import io.getquill.{ SnakeCase, SqliteJdbcContext }
+import io.getquill.context.jdbc.JdbcContext
+
+object ChatSystem {
+  def apply(config: ChatConfig)(
+      implicit commandService: CommandService,
+      eventService: EventService
+  ): Behavior[Unit] = Behaviors.setup { context =>
+    // setup database
+    val databaseProps = new Properties()
+    databaseProps.setProperty("driverClassName", config.jdbcDriver)
+    databaseProps.setProperty("jdbcUrl", config.jdbcUrl)
+    val databaseConfig = ConfigFactory.parseProperties(databaseProps)
+    implicit val database: JdbcContext[_, _] =
+      new SqliteJdbcContext(SnakeCase, databaseConfig)
+
+    // setup profiles
+    implicit val profileService: ActorRef[ProfileService.Command] = context.spawn(
+      ProfileService(config.defaultTag, config.defaultChannel),
+      ProfileService.getClass.getSimpleName
+    )
+
+    // setup global channels
+    val globalChannels = config.globalChannels map { channel =>
+
+      context.spawn(
+        GlobalChannel(name, aliases, shortcut, color, format, permission),
+        s"$name:${ GlobalChannel.getClass.getSimpleName }"
+      )
+    }
+
+    // setup local channels
+    val localChannels = config.localChannels map { channel =>
+
+      context.spawn(
+        LocalChannel(name, aliases, shortcut, color, format, radius),
+        s"$name:${ LocalChannel.getClass.getSimpleName }"
+      )
+    }
+
+    Behaviors.receiveSignal {
+      case (_, PostStop) =>
+        database.close()
+        Behaviors.stopped
+    }
+  }
+}
