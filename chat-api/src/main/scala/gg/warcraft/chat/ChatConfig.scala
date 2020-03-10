a4ea00797261b71994c6ff99f7e6f9beabdc8832
fishb6nes <fishb6nes@users.noreply.github.com>
2020-03-10 00:01:30 +0000

    Streamline initialization and parsing of config

--- chat-api/src/main/scala/gg/warcraft/chat/ChatConfig.scala
+++ chat-api/src/main/scala/gg/warcraft/chat/ChatConfig.scala
@@ -1,25 +1,6 @@
 package gg.warcraft.chat
 
 import gg.warcraft.chat.channel.{GlobalChannel, LocalChannel}
-import gg.warcraft.monolith.api.util.ColorCode
-
-final case class GlobalChannelConfig(
-    name: String,
-    aliases: Set[String],
-    shortcut: Option[String],
-    color: ColorCode,
-    format: String,
-    permission: Option[String]
-)
-
-final case class LocalChannelConfig(
-    name: String,
-    aliases: Set[String],
-    shortcut: Option[String],
-    color: ColorCode,
-    format: String,
-    radius: Float
-)
 
 case class ChatConfig(
     globalChannels: List[GlobalChannel],
--- chat-spigot/src/main/scala/gg/warcraft/chat/spigot/ChatPlugin.scala
+++ chat-spigot/src/main/scala/gg/warcraft/chat/spigot/ChatPlugin.scala
@@ -1,38 +1,81 @@
 package gg.warcraft.chat.spigot
 
+import java.util.logging.Logger
+
 import gg.warcraft.chat.{ChatConfig, ChatService}
 import gg.warcraft.chat.channel.ChannelService
 import gg.warcraft.chat.message.MessageAdapter
 import gg.warcraft.chat.profile.ChatProfileService
+import gg.warcraft.monolith.api.core.event.EventHandler
 import gg.warcraft.monolith.spigot.Codecs.Circe._
 import gg.warcraft.monolith.spigot.Implicits._
 import io.circe.generic.auto._
-import io.circe.parser._
+import io.circe.yaml.parser
+import org.bukkit.event.{HandlerList, Listener}
 import org.bukkit.plugin.java.JavaPlugin
 
+import scala.io.Source
+
 class ChatPlugin extends JavaPlugin {
+  private final val WARN_DEFAULT_CONFIG = "Falling back to default config!"
+
+  private var eventHandlers: List[EventHandler] = Nil
+  private var listeners: List[Listener] = Nil
+
   override def onLoad(): Unit = saveDefaultConfig()
 
   override def onEnable(): Unit = {
-    // chat services
+    implicit val logger: Logger = getLogger
+
+    // initialize services
     implicit val channelService: ChannelService = new ChannelService
     implicit val profileService: ChatProfileService = new ChatProfileService
     implicit val chatService: ChatService = new ChatService
 
-    // parse config
     implicit val messageAdapter: MessageAdapter = new SpigotMessageAdapter
-    decode[ChatConfig](getConfig.saveToString()) match {
-      case Left(err) =>
-        getLogger.severe("Failed to parse configuration: " + err.getMessage)
-      case Right(config) =>
-        channelService.readConfig(config)
-        profileService.readConfig(config)
+    implicit val chatEventMapper: Listener = new SpigotChatEventMapper
+
+    // read config
+    def loadConfig(config: String)(
+        implicit logger: Logger
+    ): ChatConfig = {
+      def onError(err: io.circe.Error): ChatConfig = {
+        logger severe err.getMessage
+        logger severe WARN_DEFAULT_CONFIG
+        val defaultConfig = Source.fromResource("config.yml")
+        loadConfig(defaultConfig.mkString)
+      }
+
+      parser.parse(config) match {
+        case Left(err)   => onError(err)
+        case Right(json) =>
+          json.as[ChatConfig] match {
+            case Left(err)     => onError(err)
+            case Right(config) => config
+          }
+      }
     }
 
-    // init
-    eventService.subscribe(profileService)
-    eventService.subscribe(chatService)
+    val config = loadConfig(getConfig.saveToString)
+    channelService readConfig config
+    profileService readConfig config
 
-    getServer.getPluginManager.registerEvents(new SpigotChatEventMapper, this)
+    // subscribe handlers
+    eventHandlers ::= profileService
+    eventService subscribe profileService
+
+    eventHandlers ::= chatService
+    eventService subscribe chatService
+
+    listeners ::= chatEventMapper
+    getServer.getPluginManager.registerEvents(chatEventMapper, this)
+  }
+
+  override def onDisable(): Unit = {
+    eventHandlers foreach eventService.unsubscribe
+    eventHandlers = Nil
+
+    listeners foreach HandlerList.unregisterAll
+    listeners = Nil
   }
 }
