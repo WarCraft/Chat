27e82c960d44a87a78875ecc9fad37d7fb60c359
fishb6nes <fishb6nes@users.noreply.github.com>
2020-05-03 16:30:00 +0100

    Add player object to chat events

--- chat-api/src/main/scala/gg/warcraft/chat/events.scala
+++ chat-api/src/main/scala/gg/warcraft/chat/events.scala
@@ -1,19 +1,16 @@
 package gg.warcraft.chat
 
-import java.util.UUID
-
 import gg.warcraft.monolith.api.core.event.{CancellableEvent, Event}
+import gg.warcraft.monolith.api.player.Player
 
 case class AsyncPlayerPreChatEvent(
-    playerId: UUID,
-    name: String,
+    player: Player,
     text: String,
     cancelled: Boolean = false,
     explicitlyAllowed: Boolean = false
 ) extends CancellableEvent
 
 case class AsyncPlayerChatEvent(
-    playerId: UUID,
-    name: String,
+    player: Player,
     text: String
 ) extends Event
