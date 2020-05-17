d4412dc0bff79f12c80bf5705c71310a2d4eb4a1
fishb6nes <fishb6nes@users.noreply.github.com>
2020-05-17 15:09:52 +0100

    Code cleanup

--- chat-api/src/main/scala/gg/warcraft/chat/channel/ChannelService.scala
+++ chat-api/src/main/scala/gg/warcraft/chat/channel/ChannelService.scala
@@ -3,8 +3,6 @@ package gg.warcraft.chat.channel
 import gg.warcraft.chat.ChatConfig
 import gg.warcraft.monolith.api.core.event.EventService
 
-import scala.collection.mutable
-
 class ChannelService(implicit eventService: EventService) {
   private var _channels: List[Channel] = Nil
   private var _channelsByName: Map[String, Channel] = Map.empty
@@ -19,33 +17,37 @@ class ChannelService(implicit eventService: EventService) {
   def defaultChannel: Channel = _defaultChannel
 
   def readConfig(config: ChatConfig): Unit = {
-    val channels = mutable.ListBuffer[Channel]()
-    val channelsByName = mutable.Map[String, Channel]()
-    val channelsByAlias = mutable.Map[String, Channel]()
-    val channelsByShortcut = mutable.Map[String, Channel]()
+    var channels: List[Channel] = List.empty
+    var channelsByName: Map[String, Channel] = Map.empty
+    var channelsByAlias: Map[String, Channel] = Map.empty
+    var channelsByShortcut: Map[String, Channel] = Map.empty
 
-    (config.globalChannels ++ config.localChannels) foreach { channel =>
-      channels += channel
+    (config.globalChannels ++ config.localChannels).foreach { channel =>
+      channels ::= channel
       channelsByName += (channel.name -> channel)
-      channel.aliases foreach (it => channelsByAlias += (it.toLowerCase -> channel))
-      channel.shortcut map (it => channelsByShortcut += (it.toLowerCase -> channel))
+      channel.aliases.foreach { it =>
+        channelsByAlias += (it.toLowerCase -> channel)
+      }
+      channel.shortcut.foreach { it =>
+        channelsByShortcut += (it.toLowerCase -> channel)
+      }
     }
 
     // only after successfully setting the default channel using a
     // volatile get do we overwrite the active channel collections
     _defaultChannel = channelsByName(config.defaultChannel)
 
-    _channels foreach {
+    _channels.foreach {
       case it: GlobalChannel => eventService.unsubscribe(it)
       case _                 =>
     }
 
-    _channels = channels.toList
-    _channelsByName = channelsByName.toMap
-    _channelsByAlias = channelsByAlias.toMap
-    _channelsByShortcut = channelsByShortcut.toMap
+    _channels = channels
+    _channelsByName = channelsByName
+    _channelsByAlias = channelsByAlias
+    _channelsByShortcut = channelsByShortcut
 
-    _channels foreach {
+    _channels.foreach {
       case it: GlobalChannel => eventService.subscribe(it)
       case _                 =>
     }
