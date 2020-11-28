6e6220e864a5f9cd7f4740b7f939b05eb34f5242
fishb6nes <fishb6nes@users.noreply.github.com>
2020-11-28 14:08:19 +0000

    Move migration files to api layer

--- chat-api/src/main/resources/db/migration/sqlite/V1__create_profile_table.sql
+++ chat-api/src/main/resources/db/migration/sqlite/V1__create_profile_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE profile
 (
-    player_id CHARACTER(32) PRIMARY KEY,
-    name      VARCHAR(16) NOT NULL,
+    player_id CHARACTER(32) NOT NULL PRIMARY KEY,
+    name      VARCHAR(16)   NOT NULL,
     tag       VARCHAR(32),
     home      VARCHAR(32)
 );
--- chat-spigot/src/main/resources/db/migration/V1__create_profile_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE profile
-(
-    player_id   TEXT    PRIMARY KEY,
-    name        TEXT    NOT NULL,
-    tag         TEXT,
-    home        TEXT
-);
